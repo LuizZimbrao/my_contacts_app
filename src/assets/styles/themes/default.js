5f5d50c2d2751fba62d13f67950201d2b294a557
Luiz Felipe <luizfelipezimbrao@gmail.com>
2021-11-16 16:05:09 -0300

    initial commit

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#E0e3ff',
+      light: '#6674F4',
+      main: '#5061FC',
+      dark: '#3346F0',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+    }
+  },
+
+};
---
+++ src/pages/Home/index.js
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListContainer,
+  Card,
+} from "./styles";
+
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+
+        <Link to="/new">Novo contato</Link>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Order" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Luiz Felipe</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>luizfelipe@email.com</span>
+            <span>(21) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <Link to="/edit/1">
+              <img src={edit} alt="Edit" />
+            </Link>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
